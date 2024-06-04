89bd0be88db1ab6c8edd4f4869bd077e99153995
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2024-06-04 20:54:00 +0530

    Added | Follower and User Model
    1. Create User and follower Model
    2. Fixed a bug

---
+++ GithubFollower/GithubFollower/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollower
+//
+//  Created by Akshat Chaturvedi on 04/06/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
