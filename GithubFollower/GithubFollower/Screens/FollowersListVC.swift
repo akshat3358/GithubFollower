8e8a50185e853d6b60961d9d382a6a7e9fcf2675
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2024-06-05 11:07:59 +0530

    Created | Get User's  FollowerList API

--- GithubFollower/GithubFollower/Screens/FollowersListVC.swift
+++ GithubFollower/GithubFollower/Screens/FollowersListVC.swift
@@ -16,6 +16,15 @@ class FollowersListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error!, buttonTitle: "Ok")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
