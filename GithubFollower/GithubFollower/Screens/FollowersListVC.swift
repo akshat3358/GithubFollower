a0ade881b351116841fa3041f3685e5ade16aa50
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2024-06-05 18:37:57 +0530

    Refactor | Added Result Type
    Added Result Type for Get Followers API call in completion Handler

--- GithubFollower/GithubFollower/Screens/FollowersListVC.swift
+++ GithubFollower/GithubFollower/Screens/FollowersListVC.swift
@@ -16,15 +16,17 @@ class FollowersListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error!.rawValue, buttonTitle: "Ok")
-                return
-            }
-            print("Followers.count = \(followers.count)")
-            print(followers)
-        }
         
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+                
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
