95f789671f71112e8978912b3c96f70789998902
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2024-06-14 21:49:05 +0530

    Implemented | UICollectionView Diffable Datasource

--- GithubFollower/GithubFollower/Screens/FollowersListVC.swift
+++ GithubFollower/GithubFollower/Screens/FollowersListVC.swift
@@ -5,18 +5,30 @@
 //  Created by Akshat Chaturvedi on 03/06/24.
 //
 
+/*
+ UICollectionview Diffable Datasource tutorial
+ 
+ https://www.kodeco.com/8241072-ios-tutorial-collection-view-and-diffable-data-source
+ */
+
 import UIKit
 
 class FollowersListVC: UIViewController {
 
+    enum section {
+        case main
+    }
     var username : String!
+    var followers: [Follower] = []
     var collectionView: UICollectionView!
+    var datasource: UICollectionViewDiffableDataSource<section,Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
         getFollowers()
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +45,7 @@ class FollowersListVC: UIViewController {
     func configureCollectionView(){
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
@@ -56,10 +68,31 @@ class FollowersListVC: UIViewController {
             switch result {
                 
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
+    
+    func configureDataSource(){
+        datasource = UICollectionViewDiffableDataSource<section,Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<section,Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.global().async {
+            self.datasource.apply(snapshot, animatingDifferences: true)
+        }
+        
+    }
+    
 }
