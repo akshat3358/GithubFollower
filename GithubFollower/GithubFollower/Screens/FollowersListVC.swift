c00af137b095fc57583b185e362dd67138d77089
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2024-06-23 12:35:39 +0530

    Feat | Implement Empty State Views

--- GithubFollower/GithubFollower/Screens/FollowersListVC.swift
+++ GithubFollower/GithubFollower/Screens/FollowersListVC.swift
@@ -69,6 +69,13 @@ class FollowersListVC: UIViewController {
                     hasMoreFollower.toggle()
                 }
                 self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😃"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return 
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
